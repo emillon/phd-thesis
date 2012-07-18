e6c4efa2a3ab682a28304ef98274af74a708fef7
Etienne Millon <me@emillon.org>
2012-07-18 17:19:51 +0200

    merge paras

--- code/typage-dynamique.py
+++ code/typage-dynamique.py
@@ -1,13 +1,10 @@
 >>> a = 3
->>> b = 2
 >>> c = 4.5
 >>> type(a)
 <type 'int'>
->>> type(b)
-<type 'int'>
->>> a+b
-5
->>> type(a+b)
+>>> a+a
+6
+>>> type(a+a)
 <type 'int'>
 >>> a+c
 7.5
@@ -18,10 +15,6 @@
 ... 
 >>> type(d)
 <type 'function'>
->>> d(2)
-4
->>> type(d(2))
-<type 'int'>
 >>> a+d
 Traceback (most recent call last):
   File "<stdin>", line 1, in <module>
