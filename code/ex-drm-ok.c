6df65c92ae71d2a713f6fe138625095233a837b3
Etienne Millon <me@emillon.org>
2013-07-16 17:44:15 +0200

    césures

--- code/ex-drm-ok.c
+++ code/ex-drm-ok.c
@@ -4,7 +4,7 @@ struct drm_radeon_info {
     uint32_t *value;
 };
 
-int radeon_info_ioctl(struct drm_device *dev, void *data, struct drm_file *filp)
+int radeon_info_ioctl(struct drm_device *d, void *data, struct drm_file *f)
 {
 	/*!npk userptr_fieldp data value*/
 	struct drm_radeon_info *info;
