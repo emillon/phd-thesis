72f4a881bf9d1f52b2e5545b92f3146e5b351b7d
Etienne Millon <me@emillon.org>
2014-01-22 18:03:23 +0100

    arch_ptrace

--- code/blackfin-ptrace.c
+++ code/blackfin-ptrace.c
@@ -1,3 +1,14 @@
+/* kernel/ptrace.c */
+SYSCALL_DEFINE4(ptrace, long, request, long, pid, unsigned long, addr,
+		unsigned long, data)
+{
+	struct task_struct *child = ptrace_get_task_struct(pid);
+        /* ... */
+	long ret = arch_ptrace(child, request, addr, data);
+        /* ... */
+	return ret;
+}
+
 /* arch/blackfin/kernel/ptrace.c */
 long arch_ptrace(struct task_struct *child, long request,
                  long addr, long data)
@@ -6,23 +17,23 @@ long arch_ptrace(struct task_struct *child, long request,
 	unsigned long __user *datap = (unsigned long __user *)data;
 
 	switch (request) {
-        /*...*/
+        /* ... */
 	case PTRACE_PEEKTEXT: {
 		unsigned long tmp = 0;
 		int copied;
 
 		ret = -EIO;
 
-                /*...*/
+                /* ... */
                 if (addr >= FIXED_CODE_START
 		    && addr + sizeof(tmp) <= FIXED_CODE_END) {
 			memcpy(&tmp, (const void *)(addr), sizeof(tmp));
 			copied = sizeof(tmp);
 		}
-                /*...*/
+                /* ... */
 		ret = put_user(tmp, datap);
 		break;
-        /*...*/
+        /* ... */
 	}
 
 	return ret;
