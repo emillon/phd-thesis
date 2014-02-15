a48611f8643721c18fc266296b84140d885bb346
Etienne Millon <me@emillon.org>
2014-02-15 23:12:25 +0100

    exs code

--- code/blackfin-ptrace.c
+++ code/blackfin-ptrace.c
@@ -1,40 +1,40 @@
 /* kernel/ptrace.c */
 SYSCALL_DEFINE4(ptrace, long, request, long, pid, unsigned long, addr,
-		unsigned long, data)
+                unsigned long, data)
 {
-	struct task_struct *child = ptrace_get_task_struct(pid);
+        struct task_struct *child = ptrace_get_task_struct(pid);
         /* ... */
-	long ret = arch_ptrace(child, request, addr, data);
+        long ret = arch_ptrace(child, request, addr, data);
         /* ... */
-	return ret;
+        return ret;
 }
 
 /* arch/blackfin/kernel/ptrace.c */
 long arch_ptrace(struct task_struct *child, long request,
                  long addr, long data)
 {
-	int ret;
-	unsigned long __user *datap = (unsigned long __user *)data;
+        int ret;
+        unsigned long __user *datap = (unsigned long __user *)data;
 
-	switch (request) {
+        switch (request) {
         /* ... */
-	case PTRACE_PEEKTEXT: {
-		unsigned long tmp = 0;
-		int copied;
+        case PTRACE_PEEKTEXT: {
+                unsigned long tmp = 0;
+                int copied;
 
-		ret = -EIO;
+                ret = -EIO;
 
                 /* ... */
                 if (addr >= FIXED_CODE_START
-		    && addr + sizeof(tmp) <= FIXED_CODE_END) {
-/* => */		memcpy(&tmp, (const void *)(addr), sizeof(tmp));
-			copied = sizeof(tmp);
-		}
+                    && addr + sizeof(tmp) <= FIXED_CODE_END) {
+/*=>*/                  memcpy(&tmp, (const void *)(addr), sizeof(tmp));
+                        copied = sizeof(tmp);
+                }
                 /* ... */
-		ret = put_user(tmp, datap);
-		break;
+                ret = put_user(tmp, datap);
+                break;
         /* ... */
-	}
+        }
 
-	return ret;
+        return ret;
 }
--- code/kms-buggy.c
+++ code/kms-buggy.c
@@ -1,20 +1,16 @@
-/* from drivers/gpu/drm/radeon/radeon_kms.c */
-int radeon_info_ioctl(struct drm_device *dev,
-                      void *data,
+/* drivers/gpu/drm/radeon/radeon_kms.c */
+int radeon_info_ioctl(struct drm_device *dev, void *data,
                       struct drm_file *filp) {
-	struct radeon_device *rdev =
-            dev->dev_private;
+	struct radeon_device *rdev = dev->dev_private;
 	struct drm_radeon_info *info;
-	struct radeon_mode_info *minfo =
-            &rdev->mode_info;
+	struct radeon_mode_info *minfo = &rdev->mode_info;
 	uint32_t *value_ptr;
 	uint32_t value;
 	struct drm_crtc *crtc;
 	int i, found;
 
 	info = data;
-	value_ptr = (uint32_t *)
-            ((unsigned long)info->value);
-	value = *value_ptr;
-        [...]
+	value_ptr = (uint32_t *) ((unsigned long)info->value);
+/*=>*/  value = *value_ptr;
+        /* ... */
 }
